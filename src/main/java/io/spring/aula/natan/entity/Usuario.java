f1388ef3c54cf78bc12760e0f31e79f7481442f6
Nataniel Paiva <nataniel.paiva@maisvida.med.br>
2017-09-16 17:24:53 -0300

    Subindo o projeto

---
+++ src/main/java/io/spring/aula/natan/entity/Usuario.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.spring.aula.natan.entity;
+
+/**
+ *
+ * @author Nataniel Paiva <nataniel.paiva@gmail.com>
+ */
+public class Usuario {
+
+    private String nome;
+
+    private int idade;
+
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ src/main/java/io/spring/aula/natan/service/UsuarioService.java
@@ -0,0 +1,66 @@
+/*
+*
+* Aula com Nataniel Paiva
+*
+* Esse projeto poderá ser distribuído da forma que você achar melhor
+* O importante é que você aprenda de verdade!
+*
+ */
+package io.spring.aula.natan.service;
+
+import io.spring.aula.natan.entity.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Nataniel Paiva <nataniel.paiva@gmail.com>
+ */
+@Service
+public class UsuarioService {
+
+    public List<Usuario> listaUsuario() {
+        List<Usuario> usuarioLista = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+
+        usuario.setEmail("nataniel.paiva@gmail.com");
+        usuario.setIdade(26);
+        usuario.setNome("Nataniel");
+
+        usuarioLista.add(usuario);
+
+        usuario.setEmail("fulano@gmail.com");
+        usuario.setIdade(35);
+        usuario.setNome("Fulano de tal");
+
+        usuarioLista.add(usuario);
+
+        return usuarioLista;
+
+    }
+
+    public List<Usuario> listaUsuario(Usuario usuarioAdd) {
+        List<Usuario> usuarioLista = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+
+        usuario.setEmail("nataniel.paiva@gmail.com");
+        usuario.setIdade(26);
+        usuario.setNome("Nataniel");
+
+        usuarioLista.add(usuario);
+
+        usuario.setEmail("fulano@gmail.com");
+        usuario.setIdade(35);
+        usuario.setNome("Fulano de tal");
+
+        usuarioLista.add(usuario);
+        usuarioLista.add(usuarioAdd);
+
+        return usuarioLista;
+
+    }
+
+}
